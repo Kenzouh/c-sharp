08436d0770c1310ae21fc456e45801cc86d43e1c
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-04-27 13:25:26 +0800

    feat(C#-1): add print statements

---
+++ CsPrograms/Cs1Printing.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace CsPrograms // Matches Solution Name
+{
+    class Cs1Printing // Matches File Name
+    {
+        public static void PrintStatements(string[] args)
+        {
+
+            Console.WriteLine("======== C# 1: Printing ========\n\n");
+
+            Console.WriteLine("Hello there, and welcome!");
+            Console.WriteLine("This is my very first program.");
+
+            Console.Beep();
+
+            Console.WriteLine("\nPress Enter to exit the program.");
+            Console.ReadLine(); // Waits for user input since the program closes too quickly.
+        }
+
+    }
+
+}
