0fe678a9f20fc98c6bfeff89f843c68cf9955d0c
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-04-30 16:54:51 +0800

    feat(C#-4): add constant variables

---
+++ CsPrograms/Cs4Constant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CsPrograms
+{
+    class Cs4Constant
+    {
+        public static void ConstantVars(string[] args)
+        {
+
+            Console.WriteLine("======== C# 4: Constant ========\n");
+
+            // Constant (const) = immutable values (values will never change; the same forever).
+            //                  = immutable values are known at compile time.
+
+            const int userId = 15;
+            const string deptCode = "IT134";
+            const string operationCode = "Golf Oscar"; // NATO phonetic alphabet for "Go"
+
+            Console.WriteLine("Welcome, Employee No. " + userId + "!");
+            Console.WriteLine("Your deptCode is: [" + deptCode + "]\n");
+            Console.WriteLine("Operation \"" + operationCode + "\" has commenced.");
+
+            Console.ReadKey();
+        }
+    }
+}
