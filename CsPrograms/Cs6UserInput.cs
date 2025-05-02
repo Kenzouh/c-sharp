6ab66680131c62d60c5f7bef702966984c12e39c
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-05-02 17:59:39 +0800

    feat(C#-6): add user input

---
+++ CsPrograms/Cs6UserInput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CsPrograms
+{
+    class Cs6UserInput
+    {
+        public static void UserInput(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("======== C# 6: User Input ========\n");
+
+                Console.Write("Enter your username: ");
+                String name = Console.ReadLine();
+
+                Console.Write("Enter your age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter your favorite food: ");
+                String faveFood = Console.ReadLine();
+
+                Console.WriteLine("\n====================================");
+                Console.WriteLine("||\tNice to meet you, " + name + "!\t  ||");
+                Console.WriteLine("||\tYou are " + age + " years old.\t  ||");
+                Console.WriteLine("||\tYou like eating " + faveFood + ".\t  ||");
+                Console.WriteLine("====================================");
+            }
+
+            catch(System.FormatException)
+            {
+                // Outputs upon receiving a string in variables declared as int.    
+                Console.WriteLine("\nInput NUMBERS ONLY when the program asks how old are you.");
+            }
+        }
+    }
+}
+
+
