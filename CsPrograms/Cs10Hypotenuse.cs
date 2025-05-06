696e3d4adbe79dbf814649c29ab28915c9efa0d5
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-05-06 16:53:00 +0800

    feat(C#-10): add hypotenuse calculator

---
+++ CsPrograms/Cs10Hypotenuse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CsPrograms
+{
+    class Cs10Hypotenuse
+    {
+        public static void HypotenuseCalculator(string[] args)
+        {
+            Console.WriteLine("======== C# 10: Hypotenuse Calculator ========\n");
+
+            Console.Write("Input a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Input b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            // Faster version: double c = Math.Sqrt((a * a) + (b & b));
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2)); // Slightly slower, but correct.
+
+            Console.WriteLine("==========================================");
+            Console.WriteLine("||\t\t\t\t\t||");
+
+            Console.WriteLine($"||\tHypotenuse: {c}\t||");
+            
+            Console.WriteLine("||\t\t\t\t\t||");
+            Console.WriteLine("==========================================");
+        }
+    }
+
+}
