2c02f3879518f884a7e138469bd8839d0e4f09c3
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-05-13 15:27:54 +0800

    feat(C#-17): add nested loop

---
+++ CsPrograms/Cs17NestedLoop.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CsPrograms
+{
+    class Cs17NestedLoop
+    {
+        public static void Nested(string[] args)
+        {
+            Console.WriteLine("======== C# 17: Nested Loop ========\n");
+
+            // Nested loop = loop inside of a loop. Can be for or while loop.
+            //               Uses vary, used a lot in sorting algorithms.
+
+            Console.Write("How many rows: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How many columns: ");
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a single letter/symbol: ");
+            char symbol = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++) // j = naming convention since it comes after "i".
+                {
+                    Console.Write(symbol + " ");
+                }
+
+                Console.WriteLine(""); // Moving to a next line.
+            }
+           
+        }
+    }
+}
